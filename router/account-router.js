8d5ed054c02880ca3060dcd9bec61a03084f9ef6
Kevin Carr <kevincarr@Kevins-MacBook-Pro.local>
2019-12-18 23:54:51 -0600

    created get and post endpoints

---
+++ router/account-router.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const db = require('../data/dbConfig');
+
+const router = express.Router()
+
+router.get("/", async (req, res, next) => {
+  try {
+    res.json(await db("accounts").select())
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const post = await db("accounts").where("id", req.params.id).first()
+    res.json(post)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post("/", async (req, res, next) => {
+  try {
+     const payload = {
+       name: req.body.name,
+       budget: req.body.budget
+     }
+
+     const [id] = await db("accounts").insert(payload)
+     res.json(await db("accounts").where("id", id).first())
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.put("/:id", async (req, res, next) => {
+  try {
+
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+
+  } catch (err) {
+    next(err)
+  }
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
-
-const db = require('./data/dbConfig.js');
-
+const accountRouter = require("./router/account-router.js")
 const server = express();
 
 server.use(express.json());
+server.use("/api/accounts", accountRouter)
+
+server.get("/", (req, res) => {
+  res.json({ message: "First Api build using knex" })
+})
+
+server.use((err, req, res, next) => {
+  console.log(err)
+  res.status(500).json({
+    message: "Somthing went wrong"
+  })
+})
+
 
 module.exports = server;
