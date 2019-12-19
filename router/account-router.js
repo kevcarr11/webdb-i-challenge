f5d7e4e1743aa5053fece7e0ed514391eb842a48
Kevin Carr <kevincarr@Kevins-MacBook-Pro.local>
2019-12-19 00:26:42 -0600

    All enpoints created. Mvp met

--- router/account-router.js
+++ router/account-router.js
@@ -11,7 +11,7 @@ router.get("/", async (req, res, next) => {
   }
 })
 
-router.get("/:id", async (req, res, next) => {
+router.get("/:id", validateAccountId, async (req, res, next) => {
   try {
     const post = await db("accounts").where("id", req.params.id).first()
     res.json(post)
@@ -34,20 +34,40 @@ router.post("/", async (req, res, next) => {
   }
 })
 
-router.put("/:id", async (req, res, next) => {
+router.put("/:id", validateAccountId, async (req, res, next) => {
   try {
+    const payload = {
+      name: req.body.name,
+      budget: req.body.budget
+    }
 
+    await db("accounts").where('id', req.params.id).update(payload)
+    res.json(await db("accounts").where("id", req.params.id).first())
   } catch (err) {
     next(err)
   }
 })
 
-router.delete("/:id", async (req, res, next) => {
+router.delete("/:id", validateAccountId, async (req, res, next) => {
   try {
-
+    await db("accounts").where("id", req.params.id).del()
+    res.status(204).end()
   } catch (err) {
     next(err)
   }
 })
+
+async function validateAccountId(req, res, next) {
+	try {
+		const account = await db("accounts").where("id", req.params.id).first()
+		if (account) {
+			next()
+		} else {
+			res.status(404).json({ message: "Account not found" })
+		}
+	} catch (err) {
+		next(err)
+	}
+}
 
 module.exports = router
